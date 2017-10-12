e167987092dc98ce7ccb5c5aae5783671771f44a
RonnieDipple <Ronniedipple@gmail.com>
2017-10-12 11:28:56 -0500

    lesson-02

--- app/src/main/java/com/example/ronnie/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/ronnie/coderswag/Model/Category.kt
@@ -4,4 +4,7 @@ package com.example.ronnie.coderswag.Model
  * Created by ronnie on 11/10/17.
  */
 class Category (val title: String, val image: String) {
+    override fun toString () : String{
+        return title
+    }
 }
